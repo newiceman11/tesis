c7e1621fcdaf3042ea648f0ea564fc8f279e3940
iceman0311 <jpablo382@hotmail.com>
2020-03-28 12:11:20 -0300

    menu

--- App.py
+++ App.py
@@ -1,7 +1,35 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import Menu, ttk
 from clsDB import Input
 win = tk.Tk()
+
+BarraMenu= Menu(win)
+
+win.config(menu=BarraMenu, width="300", height="300")
+
+bbddMenu=Menu(BarraMenu, tearoff=0)
+bbddMenu.add_command(label="Conectar")
+bbddMenu.add_separator()
+bbddMenu.add_command(label="Salir")
+
+borrarMenu=Menu(BarraMenu, tearoff=0)
+borrarMenu.add_command(label="Borrar campos")
+
+crudMenu=Menu(BarraMenu, tearoff=0)
+crudMenu.add_command(label="Crear")
+crudMenu.add_command(label="Leer")
+crudMenu.add_command(label="Actualizar")
+crudMenu.add_command(label="Borrar")
+
+ayudaMenu=Menu(BarraMenu, tearoff=0)
+ayudaMenu.add_command(label="Licencia")
+ayudaMenu.add_command(label="Acerca de")
+
+BarraMenu.add_cascade(label="BBDD", menu=bbddMenu)
+BarraMenu.add_cascade(label="Borrar", menu=borrarMenu)
+BarraMenu.add_cascade(label="Crud", menu=crudMenu)
+BarraMenu.add_cascade(label="Ayuda", menu=ayudaMenu)
+
 win.title("tipoDNI Input")
 def submit():
         if(nombre.get()!='' and tipoDNI.get()!=""):
@@ -34,17 +62,17 @@ fecNac = tk.StringVar()
 fecNacEntry = ttk.Entry(earn, width=13, textvariable=fecNac)
 fecNacEntry.grid(column=1, row=3,padx=10,pady=10)
 
-eLabel = ttk.Label(earn, text="Fecha Nac:").grid(column=2, row=1)
+eLabel = ttk.Label(earn, text="Email:").grid(column=2, row=1)
 tel = tk.StringVar()
 telEntry = ttk.Entry(earn, width=13, textvariable=tel)
 telEntry.grid(column=3, row=1,padx=10,pady=10)
 
-eLabel = ttk.Label(earn, text="Fecha Nac:").grid(column=2, row=2)
+eLabel = ttk.Label(earn, text="Tel:").grid(column=2, row=2)
 email = tk.StringVar()
 emailEntry = ttk.Entry(earn, width=13, textvariable=email)
 emailEntry.grid(column=3, row=2,padx=10,pady=10)
 
-eLabel = ttk.Label(earn, text="Fecha Nac:").grid(column=2, row=3)
+eLabel = ttk.Label(earn, text="Clave:").grid(column=2, row=3)
 clave = tk.StringVar()
 claveEntry = ttk.Entry(earn, width=13, textvariable=clave)
 claveEntry.grid(column=3, row=3,padx=10,pady=10)
