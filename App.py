cf1ad39eb9dfb6206b465861709ddd4b7dc82453
iceman0311 <jpablo382@hotmail.com>
2020-03-28 03:40:47 -0300

    gui basica

--- App.py
+++ App.py
@@ -1,76 +1,58 @@
 import tkinter as tk
 from tkinter import ttk
 from clsDB import Input
-
 win = tk.Tk()
-win.title("Earning Input")
+win.title("tipoDNI Input")
 def submit():
         if(nombre.get()!='' and tipoDNI.get()!=""):
-            sub_mit = Input(nombre.get(), tipoDNI.get())
+            sub_mit = Input(nombre.get(), tipoDNI.get(), numDni.get(),fecNac.get(),tel.get())
             sub_mit.setting()
             sub_mit.submit()
         else:
             print("You need to enter a value!")
     #create label frame for ui
-earn= ttk.Labelframe(win, text = "Daily Earning Input")
+earn= ttk.Labelframe(win, text = "Daily tipoDNI Input")
 earn.grid(column=0, row=0, padx=4, pady=4)
-    # create label for description
-dLabel = ttk.Label(earn, text="Nombre:").grid(column=0, row=0)
+    # create label for nombre
+dLabel = ttk.Label(earn, text="Apellido nombre:").grid(column=0, row=0)
 nombre = tk.StringVar()
 nombreEntry = ttk.Entry(earn, width=13, textvariable=nombre)
-nombreEntry.grid(column=1, row=0, padx=10, pady=10)
+nombreEntry.grid(column=1, row=0,padx=10,pady=10)
 
-eLabel = ttk.Label(earn, text=" Tipo DNI:").grid(column=0, row=1)
+eLabel = ttk.Label(earn, text="tipo DNI:").grid(column=0, row=1)
 tipoDNI = tk.StringVar()
 tipoDNIEntry = ttk.Entry(earn, width=13, textvariable=tipoDNI)
-tipoDNIEntry.grid(column=1, row=1, padx=10, pady=10)
+tipoDNIEntry.grid(column=1, row=1,padx=10,pady=10)
 
-
-eLabel = ttk.Label(earn, text="Número DNI:").grid(column=0, row=2)
-tipoDNI = tk.StringVar()
-tipoDNIEntry = ttk.Entry(earn, width=13, textvariable=tipoDNI)
-tipoDNIEntry.grid(column=1, row=2, padx=10, pady=10)
+eLabel = ttk.Label(earn, text="DNI:").grid(column=0, row=2)
+numDni = tk.StringVar()
+numDniEntry = ttk.Entry(earn, width=13, textvariable=numDni)
+numDniEntry.grid(column=1, row=2,padx=10,pady=10)
 
 eLabel = ttk.Label(earn, text="Fecha Nac:").grid(column=0, row=3)
 fecNac = tk.StringVar()
-fecNac = ttk.Entry(earn, width=13, textvariable=fecNac)
-fecNac.grid(column=1, row=3, padx=10, pady=10)
+fecNacEntry = ttk.Entry(earn, width=13, textvariable=fecNac)
+fecNacEntry.grid(column=1, row=3,padx=10,pady=10)
 
-#----------CMB--------------------------------------
-
-
-
-
-
-
-
-#----------CMB--------------------------------------
-
-
-eLabel = ttk.Label(earn, text="Email:").grid(column=2, row=1)
-email = tk.StringVar()
-email = ttk.Entry(earn, width=13, textvariable=email)
-email.grid(column=3, row=1, padx=10, pady=10)
-
-eLabel = ttk.Label(earn, text="Tel:").grid(column=2, row=2)
+eLabel = ttk.Label(earn, text="Fecha Nac:").grid(column=2, row=1)
 tel = tk.StringVar()
-tel = ttk.Entry(earn, width=13, textvariable=tel)
-tel.grid(column=3, row=2, padx=10, pady=10)
+telEntry = ttk.Entry(earn, width=13, textvariable=tel)
+telEntry.grid(column=3, row=1,padx=10,pady=10)
 
-eLabel = ttk.Label(earn, text="Clave:").grid(column=2, row=3)
+eLabel = ttk.Label(earn, text="Fecha Nac:").grid(column=2, row=2)
+email = tk.StringVar()
+emailEntry = ttk.Entry(earn, width=13, textvariable=email)
+emailEntry.grid(column=3, row=2,padx=10,pady=10)
+
+eLabel = ttk.Label(earn, text="Fecha Nac:").grid(column=2, row=3)
 clave = tk.StringVar()
-clave = ttk.Entry(earn, width=13, textvariable=clave)
-clave.grid(column=3, row=3, padx=10, pady=10)
+claveEntry = ttk.Entry(earn, width=13, textvariable=clave)
+claveEntry.grid(column=3, row=3,padx=10,pady=10)
 
 
 
-
-
-
-
-
-# create the action button
+    # create the action button
 action = ttk.Button(earn, text="submit", command=submit)
-action.grid(column=0, row=5)
+action.grid(column=0, row=4)
 win.resizable(0,0)
 win.mainloop()
--- clsDB.py
+++ clsDB.py
@@ -3,19 +3,24 @@ from datetime import datetime
 import os.path
 
 class Input:
-        os.chdir("/home/juampi/Escritorio/Python/Sistema/Programa")
-        def __init__(self, nombre, tipoDNI):
+        os.chdir("/home/juampi/Escritorio/Python/Sistema")
+        def __init__(self,nombre,tipoDNI,numDni,fecNac):
             self.nombre = nombre
             self.tipoDNI= tipoDNI
+            self.numDni= numDni
+            self.fecNac=fecNac
     #-----------------------------------------------------------------------    
         def setting(self):
-            conn = sqlite3.connect('daily_earning.db')
+            os.chdir("/home/juampi/Escritorio/Python/Sistema")
+            conn = sqlite3.connect('daily.db')
             print("Opened database successfully")
             try:
-                conn.execute('''CREATE TABLE PASAJEROS
-                     (ID INTEGER PRIMARY KEY AUTOINCREMENT,
-                     DESCRIPTION    TEXT (50)   NOT NULL,
-                     EARNING    TEXT  NOT NULL);''')
+                conn.execute('''CREATE TABLE PASAJEROS(
+                ID_PASAJERO INTEGER PRIMARY KEY AUTOINCREMENT, 
+                APELNOM VARCHAR(50),
+                TIPO_DOC VARCHAR(3),
+                NUMERO_DOC INTEGER UNIQUE,
+                FEC_NAC TIMESTAMP)''')
             except:
                 pass
             print("Table created successfully")
@@ -24,11 +29,14 @@ class Input:
         #---------------------------------------------------------------------------------------------
         def submit(self): # Insert values into earning table
             try:
-                sqliteConnection = sqlite3.connect('daily_earning.db')
+                sqliteConnection = sqlite3.connect('daily.db')
                 cursor = sqliteConnection.cursor()
                 print("Successfully Connected to SQLite")
-                count = cursor.execute("INSERT INTO PASAJEROS VALUES (NULL,'"+self.nombre+
-                "','" +self.tipoDNI+"')")
+                count=cursor.execute("INSERT INTO PASAJEROS VALUES (NULL,'"+self.nombre+
+                "','" +self.tipoDNI+
+                "','" +self.numDni+
+                "','" +self.fecNac+
+                "')")
 
                 sqliteConnection.commit()
                 print("Record inserted successfully into DAILY_EARNING_CHART table", cursor.rowcount)
