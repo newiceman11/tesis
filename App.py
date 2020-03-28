c284f50c5d627b345cf678c8699a2de5a2f41f54
iceman0311 <jpablo382@hotmail.com>
2020-03-28 02:24:39 -0300

    cambios 1

--- App.py
+++ App.py
@@ -1,12 +1,12 @@
 import tkinter as tk
 from tkinter import ttk
-from App import Input
+from clsDB import Input
 
 win = tk.Tk()
 win.title("Earning Input")
 def submit():
-        if(description.get()!='' and earning.get()!=""):
-            sub_mit = Input(description.get(), earning.get())
+        if(nombre.get()!='' and tipoDNI.get()!=""):
+            sub_mit = Input(nombre.get(), tipoDNI.get())
             sub_mit.setting()
             sub_mit.submit()
         else:
@@ -15,19 +15,62 @@ def submit():
 earn= ttk.Labelframe(win, text = "Daily Earning Input")
 earn.grid(column=0, row=0, padx=4, pady=4)
     # create label for description
-dLabel = ttk.Label(earn, text="Description:").grid(column=0, row=0)
-    # create text box for description
-description = tk.StringVar()
-descriptionEntry = ttk.Entry(earn, width=13, textvariable=description)
-descriptionEntry.grid(column=1, row=0)
-    # create label for earning
-eLabel = ttk.Label(earn, text="Earning:").grid(column=2, row=0)
-    # create text box for earning
-earning = tk.StringVar()
-earningEntry = ttk.Entry(earn, width=13, textvariable=earning)
-earningEntry.grid(column=3, row=0)
-    # create the action button
+dLabel = ttk.Label(earn, text="Nombre:").grid(column=0, row=0)
+nombre = tk.StringVar()
+nombreEntry = ttk.Entry(earn, width=13, textvariable=nombre)
+nombreEntry.grid(column=1, row=0, padx=10, pady=10)
+
+eLabel = ttk.Label(earn, text=" Tipo DNI:").grid(column=0, row=1)
+tipoDNI = tk.StringVar()
+tipoDNIEntry = ttk.Entry(earn, width=13, textvariable=tipoDNI)
+tipoDNIEntry.grid(column=1, row=1, padx=10, pady=10)
+
+
+eLabel = ttk.Label(earn, text="Número DNI:").grid(column=0, row=2)
+tipoDNI = tk.StringVar()
+tipoDNIEntry = ttk.Entry(earn, width=13, textvariable=tipoDNI)
+tipoDNIEntry.grid(column=1, row=2, padx=10, pady=10)
+
+eLabel = ttk.Label(earn, text="Fecha Nac:").grid(column=0, row=3)
+fecNac = tk.StringVar()
+fecNac = ttk.Entry(earn, width=13, textvariable=fecNac)
+fecNac.grid(column=1, row=3, padx=10, pady=10)
+
+#----------CMB--------------------------------------
+
+
+
+
+
+
+
+#----------CMB--------------------------------------
+
+
+eLabel = ttk.Label(earn, text="Email:").grid(column=2, row=1)
+email = tk.StringVar()
+email = ttk.Entry(earn, width=13, textvariable=email)
+email.grid(column=3, row=1, padx=10, pady=10)
+
+eLabel = ttk.Label(earn, text="Tel:").grid(column=2, row=2)
+tel = tk.StringVar()
+tel = ttk.Entry(earn, width=13, textvariable=tel)
+tel.grid(column=3, row=2, padx=10, pady=10)
+
+eLabel = ttk.Label(earn, text="Clave:").grid(column=2, row=3)
+clave = tk.StringVar()
+clave = ttk.Entry(earn, width=13, textvariable=clave)
+clave.grid(column=3, row=3, padx=10, pady=10)
+
+
+
+
+
+
+
+
+# create the action button
 action = ttk.Button(earn, text="submit", command=submit)
-action.grid(column=5, row=0)
+action.grid(column=0, row=5)
 win.resizable(0,0)
 win.mainloop()
--- clsDB.py
+++ clsDB.py
@@ -3,20 +3,19 @@ from datetime import datetime
 import os.path
 
 class Input:
-        os.chdir("/home/juampi/Escritorio/Python/Sistema")
-        def __init__(self, description, earning):
-            self.description = description
-            self.earning = earning
+        os.chdir("/home/juampi/Escritorio/Python/Sistema/Programa")
+        def __init__(self, nombre, tipoDNI):
+            self.nombre = nombre
+            self.tipoDNI= tipoDNI
     #-----------------------------------------------------------------------    
         def setting(self):
             conn = sqlite3.connect('daily_earning.db')
             print("Opened database successfully")
             try:
-                conn.execute('''CREATE TABLE DAILY_EARNING_CHART
+                conn.execute('''CREATE TABLE PASAJEROS
                      (ID INTEGER PRIMARY KEY AUTOINCREMENT,
                      DESCRIPTION    TEXT (50)   NOT NULL,
-                     EARNING    TEXT  NOT NULL,
-                     TIME   TEXT NOT NULL);''')
+                     EARNING    TEXT  NOT NULL);''')
             except:
                 pass
             print("Table created successfully")
@@ -28,14 +27,8 @@ class Input:
                 sqliteConnection = sqlite3.connect('daily_earning.db')
                 cursor = sqliteConnection.cursor()
                 print("Successfully Connected to SQLite")
-                count = cursor.execute("INSERT INTO PASAJEROS VALUES (NULL,'"+miNombre.get()+
-                "','" +miTipo.get()+
-                "','" +miNum.get()+
-                "','" +miFec.get()+
-                "','" +miPais.get()+
-                "','" +miTel.get()+
-                "','" +miEmail.get()+
-                "','" +miClave.get()+"')")
+                count = cursor.execute("INSERT INTO PASAJEROS VALUES (NULL,'"+self.nombre+
+                "','" +self.tipoDNI+"')")
 
                 sqliteConnection.commit()
                 print("Record inserted successfully into DAILY_EARNING_CHART table", cursor.rowcount)
